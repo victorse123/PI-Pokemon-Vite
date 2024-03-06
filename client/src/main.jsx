207ae57be5cfa58ff606995ba1fec4825c20061c
Victor Sepulveda <victorseva123@gmail.com>
2024-03-06 11:43:43 -0500

    Configuracion y pruebas vite 06/03/24

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./Redux/store/store.js";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+// )
