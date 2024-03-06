12ebfc5581ed3c39a5561bab9a49c9fc9b54da2b
Victor Sepulveda <victorseva123@gmail.com>
2024-03-06 16:36:32 -0500

    Configuracion y pruebas vite 06/03/24

--- client/src/Redux/store/store.js
+++ client/src/Redux/store/store.js
@@ -1,13 +1,47 @@
+// import { createStore, applyMiddleware, compose } from "redux";
+
+// import thunkMiddleware from "redux-thunk";
+
+// import reducer from "../reducers/Reducers";
+
+// const composeEnHancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   reducer,
+//   composeEnHancer(applyMiddleware(thunkMiddleware)) // esta línea es para poder hacer peticiones a un server
+// );
+// export default store;
+
+// import { createStore, applyMiddleware, compose } from "redux";
+// import rootReducer from "..//reducers/Reducers";
+// import thunk from "redux-thunk";
+
+// // la extensión de Redux DevTools solo se active en el entorno de desarrollo y no en producción
+// const composeEnhancers =
+//   (process.env.NODE_ENV === "development" &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+
+// export default store;
+
+
 import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/Reducers";
+import * as thunk from "redux-thunk";
 
-import thunkMiddleware from "redux-thunk";
-
-import reducer from "../reducers/Reducers";
-
-const composeEnHancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  (process.env.NODE_ENV === "development" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const store = createStore(
-  reducer,
-  composeEnHancer(applyMiddleware(thunkMiddleware)) // esta línea es para poder hacer peticiones a un server
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk.default || thunk))
 );
+
 export default store;
