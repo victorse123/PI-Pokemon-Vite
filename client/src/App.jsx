b529890889dc8f71b7b0a482c513e586b72e3b06
Victor Sepulveda <victorseva123@gmail.com>
2024-03-06 13:32:53 -0500

    Configuracion y pruebas vite 06/03/24

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,7 @@ import Nav from './components/nav/Nav.jsx'
 import LadingPag from './components/landingpag/LandingPag.jsx';
 import Cards from "./components/cards/Cards.jsx"
 import Create from "./components/create/Create.jsx"
-import { Route, Switch, useLocation} from 'react-router-dom';
+import { Route, Routes, useLocation} from 'react-router-dom';
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { addAllPokemon, addAllTypes } from './Redux/actions/actions.js';
@@ -62,12 +62,12 @@ function App() {
   return (
     <div className="appDiv">
       {location.pathname !== '/' && <Nav setPagina={setPagina}/>}
-      <Switch>
+      <Routes>
         <Route path="/" element={<LadingPag allPokemon={allPokemon}/>} />
         <Route path="/home"  element={<Cards pagina={pagina} setPagina={setPagina}/>}/>
         <Route path="/create" element={<Create/>} />
         <Route path='detail/:idDetail' element={<Detail/>} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
