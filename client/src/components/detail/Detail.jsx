9f4c4386b189bd30dde2f71e1497947d1daa2e90
Victor Sepulveda <victorseva123@gmail.com>
2024-03-06 13:06:21 -0500

    Configuracion y pruebas vite 06/03/24

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 import { addPokDetail } from "..//..//Redux/actions/actions";
 import styles from '../detail/Detail.module.css'
-import { imageTypes } from "..//..//assets/Image/imageImg";
-import { imageImg } from "..//..//assets/Image/imageTypes";
+import { imageTypes } from "..//..//assets/Image/imageTypes";
+import { imageImg } from "..//..//assets/Image/imageImg";
 
 const Detail = ()=>{
     const {detail} = useSelector((state)=> state)
